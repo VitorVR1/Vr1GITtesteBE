73859629428f54d7793f98b58b3f0a164a6338c0
VitorVR1 <vitorvr1@hotmail.com>
2023-09-21 19:03:55 -0300

    sdsdd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 // Cria um servidor HTTP que responde com "Olá, mundo!" para todas as solicitações
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Olá, mundo!\n');
+  res.end('Olá, mundo eu sou o back end!\n');
 });
 
 // O servidor escuta na porta 3001
